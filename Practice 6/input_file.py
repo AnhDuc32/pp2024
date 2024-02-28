6de71720a3be377a98728051a165fc0eaf01e6c8
AnhDuc32 <anhducnguyen3204@gmail.com>
2024-02-28 21:41:25 +0700

    Practice 6 - load data from students.txt and courses.txt

---
+++ Practice 6/input_file.py
@@ -0,0 +1,70 @@
+from Domains.student import Student
+from Domains.course import Course
+import numpy
+import pickle
+
+def student_number():
+    while (True):
+        student_num = int(input("Enter number of students: "))
+        if student_num > 0:
+            print(f'\nThe number of students is {student_num}')
+            break
+        else:
+            print("Invalid input! Try again\n")
+    return student_num
+def student_information(student_num):
+    if student_num == 0:
+        print("There are currently no students\n")
+        return None
+    f = open('students.txt', 'wb')
+    student_info = []
+    for i in range(student_num):
+        print(f'Student {i+1}: ')
+        id = input("Enter ID: ")
+        name = input("Enter name: ")
+        dob = input("Enter DoB: ")
+        print("")
+        student = Student(id, name, dob) 
+        student_info.append(student)
+    pickle.dump(student_info, f)
+    f.close()
+    return student_info
+def student_mark(course_num, student, student_num):
+    if course_num == 0 or student_num == 0:
+        return None
+    else:
+        f = open('marks.txt', 'wb')
+        for i in range (student_num):
+            student[i]._mark = numpy.array([])
+        for c in range (course_num):
+            print(f'\nCourse {c+1}:')
+            for j in range (student_num):
+                student[j]._mark = numpy.append(student[j]._mark, float(input(f'Enter mark for student {j+1}: ')))
+                pickle.dump(student[j]._mark, f)
+        f.close()
+
+def course_number():
+    while (True):
+        course_num = int(input("Enter number of courses: "))
+        if course_num > 0:
+            print(f'\nThe number of courses is {course_num}')
+            break
+        else:
+            print("Invalid input! Try again\n")
+    return course_num
+def course_information(course_num):
+    if course_num == 0:
+        print("There are currently no courses\n")
+    f = open('courses.txt', 'wb')
+    course_info = []
+    for i in range(course_num):
+        print(f'Course {i+1}: ')
+        id = input("Enter ID: ")
+        name = input("Enter name: ")
+        credit = int(input("Enter credit: "))
+        print("")
+        course = Course(id, name, credit) 
+        course_info.append(course)
+    pickle.dump(course_info, f)
+    f.close()
+    return course_info
---
+++ Practice 6/run_file.py
@@ -0,0 +1,117 @@
+import os
+import zipfile
+import pickle
+import numpy as np
+import input_file as i
+import output_file as o
+
+def compress_file():
+    file_name = ['students.txt', 'courses.txt', 'marks.txt']
+    compress_file_name = zipfile.ZipFile('students.dat', 'w')
+    for f in file_name:
+        compress_file_name.write(f, compress_type=zipfile.ZIP_DEFLATED)
+    compress_file_name.close()
+
+def decompress_file(student, course, student_num, course_num):
+    file_name = 'students.dat'
+    if os.path.exists(file_name):
+        print('"Students.dat" is already exists!\nLoading data successfully!')
+        with zipfile.ZipFile(file_name, 'r') as zip_file:
+            zip_file.extractall()
+            with open('students.txt', 'rb') as stu_data:
+                stu = pickle.load(stu_data)
+                for j in range (len(stu)):
+                    student_num += 1
+                    student.append(stu[j])
+            with open('courses.txt', 'rb') as course_data:
+                cour = pickle.load(course_data)
+                for j in range (len(cour)):
+                    course_num += 1
+                    course.append(cour[j])
+            with open('marks.txt', 'rb') as mark_data:
+                mark = pickle.load(mark_data)
+                for j in range (len(mark)):
+                    student[j]._mark = np.append(student[j]._mark, mark[j])
+    else:
+        print('"Students.dat" is not exists!')
+    return student, course, student_num, course_num
+
+def clear():
+    print("")
+    os.system("pause")
+    os.system("cls")
+
+def main():
+    student = []
+    course = []
+    student_num = 0
+    course_num = 0
+    
+    student, course, student_num, course_num = decompress_file(student, course, student_num, course_num)
+    
+    while (True):
+        print("""
+===========================================================
+1. Input number of students in a class
+2. Input student information: id, name, DoB
+3. Input number of courses
+4. Input course information: id, name, credit
+5. Select a course, input marks for student in this course
+6. List courses
+7. List students
+8. Show student marks for a given course
+9. List students by GPA in descending order
+0. Exit the program 
+===========================================================""")
+
+        option = int(input("Choose an option: "))
+        if option == 0:
+            compress_file()
+            os.system("cls")
+            print("""
+You have exited the program. All the files have been compressed into "students.dat".
+GOODBYE!""")
+            exit(0)
+        elif option == 1:
+            os.system("cls")
+            student_num = i.student_number()
+            clear()
+        elif option == 2:
+            os.system("cls")
+            student = i.student_information(student_num)
+            clear()
+        elif option == 3:
+            os.system("cls")
+            course_num = i.course_number()
+            clear()
+        elif option == 4:
+            os.system("cls")
+            course = i.course_information(course_num)
+            clear()
+        elif option == 5:
+            os.system("cls")
+            o.student_list(student, student_num)
+            o.course_list(course, course_num)
+            i.student_mark(course_num, student, student_num)
+            clear()
+        elif option == 6:
+            os.system("cls")
+            o.course_list(course, course_num)
+            clear()
+        elif option == 7:
+            os.system("cls")
+            o.student_list(student, student_num)
+            clear()
+        elif option == 8:
+            os.system("cls")
+            o.course_list(course, course_num)
+            o.student_list(student, student_num)
+            o.show_student_mark(course, course_num, student, student_num)
+            clear()
+        elif option == 9:
+            os.system("cls")
+            o.cal_gpa(student, student_num, course, course_num)
+            clear()
+
+if __name__ == "__main__":
+    main()
